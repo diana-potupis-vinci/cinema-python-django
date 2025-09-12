223f87ef70fc325da5953972de7d3b548d26eb89
Diana Potupis <diana.potupis@student.vinci.be>
2025-09-12 09:16:38 -0400

    feat: enhance admin interface and models for Film, Actor, Genre, Seance, and Reservation

--- demo_app/admin.py
+++ demo_app/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 
-from .models import Film
+from .models import Film, Genre, Actor, Seance, Reservation
 
-# Register your models here.
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ('name',)
 
-admin.site.register(Film)
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_name', 'first_name')
+    search_fields = ('last_name', 'first_name')
+
+@admin.register(Film)
+class FilmAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'genre', 'duration')
+    list_filter = ('genre',)
+    search_fields = ('title',)
+    filter_horizontal = ('actors',)
+
+@admin.register(Seance)
+class SeanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'film', 'date', 'time', 'hall')
+    list_filter = ('film', 'hall', 'date', 'time')
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'seance', 'places', 'reservation_date')
+    list_filter = ('seance', 'user')
