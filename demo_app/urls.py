864cc7748cbe6fd6f6108404b113f54fa7f910c0
Diana potupis <diana-potupis@student.vinci.be>
2025-09-16 14:05:54 -0400

    feat: add list of films

--- demo_app/urls.py
+++ demo_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
-    path('film/list/', views.FilmsListView.as_view(), name="film-list"),
+    path('films/', views.FilmsListView.as_view(), name="film-list"),
     path('films/<int:id>/', views.FilmView.as_view(), name="film_detail"),
 ]
