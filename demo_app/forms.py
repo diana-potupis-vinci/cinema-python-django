4bb1d4f33ef6e1f4e23f789a953a53c1b25fd3eb
Diana Potupis <diana.potupis@student.vinci.be>
2025-09-19 10:19:26 -0400

    feat: add username field to user registration form

--- demo_app/forms.py
+++ demo_app/forms.py
@@ -7,6 +7,7 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
+    username = forms.CharField(max_length=30, required=True)
 
     class Meta:
         model = User
